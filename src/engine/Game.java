0eeb80098e7761f002c90e66c46248be86eb6ae9
Yossif Alaa <yossif21299@gmail.com>
2025-02-26 10:26:53 +0200

    work on 4.19 board & 4.20 game

---
+++ src/engine/Game.java
@@ -0,0 +1,8 @@
+package engine;
+import engine.board.BoardManager;
+import engine.board.Board;
+import java.util.ArrayList;
+public class Game {
+final Board board;
+
+}
--- src/engine/board/Board.java
+++ src/engine/board/Board.java
@@ -1,5 +1,40 @@
 package engine.board;
-
+import java.util.ArrayList;
+import engine.GameManager;
 public class Board {
-
+	private final GameManager gameManager;
+	private final ArrayList<Cell> track;
+	private final ArrayList<SafeZone> safeZones;
+	private int splitDistance;
+	
+	Board(ArrayList<Colour> colourOrder, GameManager gameManager){
+		this.gameManager = gameManager;
+		track = new ArrayList<>();
+		safeZones = new ArrayList<>();
+		splitDistance = 3;
+		//constructor stuck at 4.
+	}
+	private void assignTrapCell() {
+		/* void assignTrapCell() should randomize a cell position on the track and flag it as a trap cell.
+		The position must be a NORMAL cell and not flagged as trap already. */
+	}
+	
+	
+	
+	
+	
+	public int getSplitDistance() {
+		return splitDistance;
+	}
+	public void setSplitDistance(int splitDistance) {
+		this.splitDistance = splitDistance;
+	}
+	public ArrayList<Cell> getTrack() {
+		return track;
+	}
+	public ArrayList<SafeZone> getSafeZones() {
+		return safeZones;
+	}
+	
+	
 }
