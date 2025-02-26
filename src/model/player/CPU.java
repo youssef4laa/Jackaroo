c653393a143796fa85e4b753f842e8e1109a36c2
Yossif Alaa <yossif21299@gmail.com>
2025-02-26 10:13:23 +0200

    4.19 created CPU class

---
+++ src/model/player/CPU.java
@@ -0,0 +1,19 @@
+package model.player;
+import engine.board.BoardManager;
+import model.Colour;
+
+public class CPU extends Player{
+	BoardManager boardManager;
+	/**
+	 * 
+	 * @param name represents name of the CPU player
+	 * @param colour represents colour associated with CPU
+	 * @param boardManager current board instance.
+	 */
+
+	public CPU(String name, Colour colour, BoardManager boardManager) {
+		super(name, colour);
+		this.boardManager = boardManager;
+	}
+	
+}
