2fd28c45ebec94327bd753012db642a4cc797cf8
Yossif Alaa <yossif21299@gmail.com>
2025-02-26 22:46:27 +0200

    More javadocs updates

--- src/model/Colour.java
+++ src/model/Colour.java
@@ -2,7 +2,19 @@ package model;
 
 /**
  * Represents the four possible colours of the marbles in the Jackaroo game.
+ * Each colour is associated with a player and their marbles, safe zones, and entry points.
  */
 public enum Colour {
-    RED, GREEN, BLUE, YELLOW
+    
+    /** Represents the red colour for marbles and player zones. */
+    RED, 
+    
+    /** Represents the green colour for marbles and player zones. */
+    GREEN, 
+    
+    /** Represents the blue colour for marbles and player zones. */
+    BLUE, 
+    
+    /** Represents the yellow colour for marbles and player zones. */
+    YELLOW
 }
--- src/model/card/standard/Suit.java
+++ src/model/card/standard/Suit.java
@@ -1,8 +1,20 @@
 package model.card.standard;
 
 /**
- * Enum representing the four suits of standard playing cards.
+ * Enum representing the four suits of standard playing cards in the Jackaroo game.
+ * Suits are used in conjunction with standard cards to determine gameplay actions.
  */
 public enum Suit {
-    HEART, DIAMOND, SPADE, CLUB
+    
+    /** Represents the Heart suit. */
+    HEART, 
+    
+    /** Represents the Diamond suit. */
+    DIAMOND, 
+    
+    /** Represents the Spade suit. */
+    SPADE, 
+    
+    /** Represents the Club suit. */
+    CLUB
 }
