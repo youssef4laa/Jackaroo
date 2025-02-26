2766cbf0442e29b2f241164b0d2ca8f03dfa7f04
Yossif Alaa <yossif21299@gmail.com>
2025-02-26 10:32:14 +0200

    Update Game.java

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -3,6 +3,12 @@ import engine.board.BoardManager;
 import engine.board.Board;
 import java.util.ArrayList;
 public class Game {
-final Board board;
-
+private final Board board;
+private final ArrayList<Player> players;
+private final ArrayList<Card> firePit;
+private int currentPlayerIndex;
+private int turn;
+Game(String playerName) throws IOException{
+	
+}
 }
