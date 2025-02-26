1492686ce7c222d791530c4f995e3654937b104d
Yossif Alaa <yossif21299@gmail.com>
2025-02-26 09:47:35 +0200

    Create Player.java

---
+++ src/model/player/Player.java
@@ -0,0 +1,14 @@
+package model.player;
+import java.util.ArrayList;
+import model.Colour;
+import model.card.Card;
+
+public class Player {
+private final String name;
+private final Colour colour;
+private ArrayList<Card> hand;
+private ArrayList<Marble> marbles;
+private Card selectedCard;
+private ArrayList<Marble> selectedMarbles;
+
+}
